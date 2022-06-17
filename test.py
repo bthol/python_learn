e4a4b276336dcdb9ae06984bf646616f939e6832
bthol <95544562+bthol@users.noreply.github.com>
2022-06-16 17:30:34 -0700

    convert number to string

--- test.py
+++ test.py
@@ -1,41 +1,44 @@
 # string concatenation
-name = "Blake"
-stringy = "Hello, my name is " + name
+# name = "Blake"
+# stringy = "Hello, my name is " + name
 # print(stringy)
 
 # variabes and reassinment
-x = "original string"
+# x = "original string"
 # print(x)
-x = "new string"
+# x = "new string"
 # print(x)
 
 # functions, arguments and calling functions
-def funct1(y):
-    z = y
-    print(z)
+# def funct1(y):
+#     z = y
+#     print(z)
 # funct1("pass")
 
 # arrays and accessing items
-arr = [-1, -2, 2, 1]
-arr7 = [7, 7, 7, 7, 7, 7, 7, 0]
+# arr = [-1, -2, 2, 1]
+# arr7 = [7, 7, 7, 7, 7, 7, 7, 0]
 # print(arr[sum(arr) - 3])
 # print(arr[len(arr) - 1])
 # print(arr7[len(arr7) - 1])
 
 #conditional statements
-condition = 1
+# condition = 1
 # condition = 2
 # condition = "carrots"
-if condition == 1:
-    print("First")
-elif condition == 2: 
-    print("Second")
-else:
-    print("else")
+# if condition == 1:
+#     print("First")
+# elif condition == 2: 
+#     print("Second")
+# else:
+#     print("else")
 
 #testing data types with conditional statements
-con = 1
-if con == "1":
-    print("string")
-elif con == 1:
-    print("number")
+# con = 1
+# if con == "1":
+#     print("string")
+# elif con == 1:
+#     print("number")
+
+#converting numbers into strings
+print(str(42) + " and a string")
